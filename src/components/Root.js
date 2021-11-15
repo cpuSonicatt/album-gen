55de3dcfd7bc90b72575cf237b7585004e119191
cpuSonicatt <18102974+cpuSonicatt@users.noreply.github.com>
2021-11-15 18:52:47 +0000

    Improvements lol

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Suspense } from 'react';
 import "../style/root.css"
 import Generate from "name-creator"
 import RandPerson from "node-random-name"
-import { uniqueNamesGenerator, adjectives, animals, colors, languages } from "unique-names-generator"
-import { Person, Album, Cached } from "@material-ui/icons"
+import { uniqueNamesGenerator, adjectives, animals, colors } from "unique-names-generator"
+import { prominent } from 'color.js'
+import { Cached } from "@material-ui/icons"
+import chroma from "chroma-js";
+import AlbumChanceTable from '../js/AlbumChanceTable';
+import webfont from 'webfontloader';
+
+
+const AlbumInfo = React.lazy(() => import("./AlbumInfo"));
+const API_KEY = 'AIzaSyDDiO8nLVRMDaXwrJp61Cdcar5gFmmiR1Q'; //change me
+
+
 
 // eslint-disable-next-line no-extend-native
 String.prototype.toProperCase = function () {
@@ -26,9 +36,10 @@ Array.prototype.shuffle = function() {
        this[j] = temp;
     }
     return this;
-  }
+}
 
-export default class Root extends Component {
+
+export default class Root extends React.Component {
 
     constructor() {
         super()
@@ -36,7 +47,11 @@ export default class Root extends Component {
             cover: "",
             album: "",
             artist: "",
-            isLoad: false
+            gradTop: "#F7F4F3",
+            gradBot: "#F7F4F3",
+            tColTop: "",
+            tColBot: "",
+            font: ""
         }
 
     }
@@ -45,30 +60,47 @@ export default class Root extends Component {
         this.generate()
     }
 
-    generate = () => {
-        this.generateCover()
-        this.generateAlbum()
+    generate = async () => {
+        this.setState({
+            cover: "",
+            album: "",
+            artist: "",
+            gradTop: "#F7F4F3",
+            gradBot: "#F7F4F3",
+            tColTop: "",
+            tColBot: "",
+            font: ""
+        })
+        await this.generateCover()
         this.generateArtist()
+        this.generateAlbum()
+        this.genereateFont()
     }
 
-    generateCover = () => {
+    generateCover = async () => {
+
+        let id = await (await fetch("https://picsum.photos/1000?random=" + Math.random())).headers.get("picsum-id")
+        let url = `https://picsum.photos/id/${id}/1000/1000`
+
+
+        const colours = await prominent(url, { amount: 2, format: "hex"})
+
         this.setState({
-            cover: "https://picsum.photos/1000?random=" + Math.random() 
+            cover: url,
+            gradTop: chroma(colours[0]).desaturate().hex(),
+            gradBot: chroma(colours[1]).desaturate().hex(),
+            tColTop: this.getTextColour(colours[0]),
+            tColBot: this.getTextColour(colours[1])
         })
     }
 
     generateAlbum = () => {
-        if (Math.random().between(0,0.75)) {
-            let dict =  [adjectives, animals, colors, adjectives, animals, colors, languages].shuffle()
-            this.setState({
-                album: uniqueNamesGenerator({length: Math.ceil(Math.random() * 3) ,dictionaries: dict, style: "capital", separator: " "})
-            })
-        } else {
-            this.setState({
-                album: Generate({words: 1 + Math.random() * 3}).spaced.toProperCase()
-            })
-            
-        }
+
+        const act = new AlbumChanceTable(this.state.artist)
+        this.setState({
+            album: act.get()
+        })
+
     }
 
     generateArtist = () => {
@@ -89,29 +121,61 @@ export default class Root extends Component {
         })
     }
 
+    genereateFont = async () => {
+        try {
+            const result = await fetch('https://www.googleapis.com/webfonts/v1/webfonts?key=' + API_KEY)
+            const data = await result.json()
+            let randomFont = data.items[Math.floor(Math.random() * data.items.length)].family
+            console.log(randomFont)
+            webfont.load({
+                google: {
+                    families: [randomFont]
+                }
+            });
+            this.setState({
+                font: randomFont
+            })
+        } catch (error) {
+            console.log('loadFontsList', error, error.message);
+        }
+    }
+
+    getTextColour(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        var col = {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        };
+        if ((col.r * 0.299) + (col.g * 0.587) + (col.b * 0.114) > 186) {
+            return "black" // change me
+        } else {
+            return "white"
+        }
+    }
+
     render() {
+        document.body.style.background = `linear-gradient(${this.state.gradTop}, ${this.state.gradBot}) fixed`;
         return (
             <div>
-                <h1>This album doesn't exist.</h1>
-                <div className="center">
-                    {this.state.isLoad ? null :
-                        <div className="full"/>
-                    }
-                    <img className="cover" onLoad={() => this.setState({isLoad: true})} src={this.state.cover} alt=""/>
-                    <div className="details">
-                        <Album className="icon"/><p>{this.state.album}</p>
-                    </div>
-                    <div className="details">
-                        <Person className="icon"/><p>{this.state.artist}</p>
-                    </div>
+                <div>
+                    <h1 style={{color: this.state.tColTop}}>This album doesn't exist.</h1>
+                    <Suspense fallback={<h1 className="black">loading fuck fuck fuck fuck fuck </h1>}>
+                        <AlbumInfo
+                            cover={this.state.cover}
+                            album={this.state.album} showAlbum={Math.random() < 0.75}
+                            artist={this.state.artist}
+                            textCol={this.state.tColBot}
+                            font={this.state.font}
+                        />
+                    </Suspense>
                 </div>
                 
                 <div className="menu">
                     <li>
-                        <p className="menu-p foot"><i>Photos from <a href="https://picsum.photos/">Lorem Picsum</a></i></p>  
-                        <p className="menu-p right">Generate another banger:</p>
+                        <p className="menu-p foot white"><i>Photos from <a href="https://picsum.photos/">Lorem Picsum</a></i></p>  
+                        <p className="menu-p right white">Generate another banger:</p>
                         <ul><button onClick={() => this.generate()}><Cached className="menu-icon"/></button></ul>
-                                          
                     </li>
                 </div>
             </div>
